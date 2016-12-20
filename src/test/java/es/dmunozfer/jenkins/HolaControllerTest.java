f328c5270a6bd4d3880eb1f4ccc314a022963401
Marina Blanco <marina-11416@hotmail.com>
2016-12-20 19:31:10 +0100

    Commit de prueba

--- src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
+++ src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
@@ -46,7 +46,7 @@ public class HolaControllerTest {
 	.andExpect(content().string(containsString("¡Hola Mundo!")))
 	;
     }
-    //Comiiiiitt
+    //Comiiiiittaa
     @Test
     public void holaDavid() throws Exception {
 	mockMvc.perform(get("/hola?nombre=David"))
